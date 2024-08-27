a37fea8c481b05a4a5d565dbc03945e974e94e12
f2rkan <furkanrolla@icloud.com>
2024-08-27 14:00:56 +0300

    updated algorithm

--- src/main/java/com/example/musteri_bakiye_seyri/service/MusteriService.java
+++ src/main/java/com/example/musteri_bakiye_seyri/service/MusteriService.java
@@ -27,27 +27,58 @@ public class MusteriService {
         return musteriRepository.findAll();
     }
 
+//    public Date getMaxBorcluTarihi(Long musteriId) {
+//    	List<Fatura> faturalar = faturaRepository.findByMusteriId(musteriId);
+//
+//        List<Fatura> unpaidFaturalar = faturalar.stream()
+//                .filter(fatura -> fatura.getOdemeTarihi() == null)
+//                .sorted(Comparator.comparing(Fatura::getFaturaTarihi))
+//                .collect(Collectors.toList());
+//
+//        double toplamBorç = 0.0;
+//        double maxBorç = 0.0;
+//        Date maxBorçTarihi = null;
+//
+//        for (Fatura fatura : unpaidFaturalar) {
+//            toplamBorç += fatura.getFaturaTutari();
+//
+//            if (toplamBorç > maxBorç) {
+//                maxBorç = toplamBorç;
+//                maxBorçTarihi = fatura.getFaturaTarihi();
+//            }
+//        }
+//
+//        return maxBorç > 0 ? maxBorçTarihi : null;
+//    }
+    
+    //----
+
     public Date getMaxBorcluTarihi(Long musteriId) {
-    	List<Fatura> faturalar = faturaRepository.findByMusteriId(musteriId);
+        List<Fatura> faturalar = faturaRepository.findByMusteriId(musteriId);
 
-        List<Fatura> unpaidFaturalar = faturalar.stream()
-                .filter(fatura -> fatura.getOdemeTarihi() == null)
-                .sorted(Comparator.comparing(Fatura::getFaturaTarihi))
-                .collect(Collectors.toList());
+        Map<Date, Double> borcTarihleri = new TreeMap<>();
 
-        double toplamBorç = 0.0;
-        double maxBorç = 0.0;
-        Date maxBorçTarihi = null;
+        for (Fatura fatura : faturalar) {
+            borcTarihleri.put(fatura.getFaturaTarihi(), borcTarihleri.getOrDefault(fatura.getFaturaTarihi(), 0.0) + fatura.getFaturaTutari());
 
-        for (Fatura fatura : unpaidFaturalar) {
-            toplamBorç += fatura.getFaturaTutari();
-
-            if (toplamBorç > maxBorç) {
-                maxBorç = toplamBorç;
-                maxBorçTarihi = fatura.getFaturaTarihi();
+            if (fatura.getOdemeTarihi() != null) {
+                borcTarihleri.put(fatura.getOdemeTarihi(), borcTarihleri.getOrDefault(fatura.getOdemeTarihi(), 0.0) - fatura.getFaturaTutari());
             }
         }
 
-        return maxBorç > 0 ? maxBorçTarihi : null;
+        double toplamBorc = 0.0;
+        Date maxBorcTarihi = null;
+        double maxBorc = Double.MIN_VALUE;
+
+        for (Map.Entry<Date, Double> entry : borcTarihleri.entrySet()) {
+            toplamBorc += entry.getValue();
+
+            if (toplamBorc > maxBorc) {
+                maxBorc = toplamBorc;
+                maxBorcTarihi = entry.getKey();
+            }
+        }
+        System.out.println(maxBorc);
+        return maxBorcTarihi;
     }
 }
